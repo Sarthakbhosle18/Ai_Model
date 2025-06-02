7f85b83773f6b14e936d9fc27ac846d2c7bbfc1e
Sarthakbhosle18 <154578679+Sarthakbhosle18@users.noreply.github.com>
2025-06-02 15:14:36 +0530

    new

---
+++ C++/C++ Tutorial 1.cpp
@@ -0,0 +1,13 @@
+ #include<iostream>
+using namespace std;
+int m = 10;
+ int main()
+ {
+ 	int k = m;
+ 	int m = 30;
+ 	cout<<"k = "<<m<<endl;
+	 cout<<"m = "<<m<<endl;
+	 cout<<"k = "<<m<<endl;
+	 cout<<":m = "<<::m<<endl;
+	
+ }
